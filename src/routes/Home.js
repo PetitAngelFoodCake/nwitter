d21dcdc86334cdd0fa68fae88c50b61a9baa26de
PetitAngelFoodCake <angelfoodcake@naver.com>
2022-04-14 23:48:53 +0900

    최종 완성 코드

---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { dbService } from "fbase";
+import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
+
+const Home = ({ userObj }) => {    
+    const [nweets, setNweets] = useState([]);    
+    
+    useEffect(() => {
+        dbService
+          .collection("nweets")
+          .orderBy("createdAt", "desc")
+          .onSnapshot((onSnapshot) => {
+            const newArray = onSnapshot.docs.map((document) => ({
+                id: document.id,
+                ...document.data(),
+            }));
+            setNweets(newArray);
+        });
+    }, []);
+
+    console.log(nweets);
+   
+    return(
+      <>
+      <div className="container">
+        <NweetFactory userObj={userObj} />
+        <div style={{ marginTop: 30 }}>
+            {nweets.map((nweet) => (                
+                <Nweet 
+                    key={nweet.id}
+                    nweetObj={nweet}
+                    isOwner={nweet.creatorId === userObj.uid}
+                />
+            ))}
+        </div>
+      </div>
+      </>
+    );
+};
+
+export default Home;
